6c9f2414edcf683b4782577eee38bb7339b5fe15
priyanshu <priyanshusengarps@gmail.com>
2024-11-08 22:54:15 +0530

    Basic JAVA Programs

--- src/Program5.java
+++ src/Program5.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
 public class Program5 {
- public static void main(String[] args){
-     Scanner sc=new Scanner(System.in);
-     System.out.println("Enter the First Number-");
-     int num=sc.nextInt();
-     System.out.println("Enter the Second Number-");
-     int num2=sc.nextInt();
-     System.out.println(num*num2);
- }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the First Number-");
+        int num= sc.nextInt();
+        System.out.println("Enter the Second Number-");
+        int num2= sc.nextInt();
+        System.out.println(num*num2);
+    }
 }
