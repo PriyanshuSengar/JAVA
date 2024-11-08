c6167fd7b1b3eefc8db9b16b1d1ca1baf9487655
priyanshu <priyanshusengarps@gmail.com>
2024-11-08 22:49:29 +0530

    Basic JAVA Programs

---
+++ src/Program2.java
@@ -0,0 +1,9 @@
+public class Program2 {
+    public static void main(String[] args){
+        int a,b;
+        a=10;
+        b=20;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
