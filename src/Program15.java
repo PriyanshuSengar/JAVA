593e27f26d98b0e7f4d7336ef723efb3cd4910f5
priyanshu <priyanshusengarps@gmail.com>
2024-11-10 10:50:00 +0530

    Basic JAVA Programs

---
+++ src/Program15.java
@@ -0,0 +1,13 @@
+public class Program15 {
+    public static void  main(String[] args){
+        int a=10,b=4,c=0;
+       c=b;
+      b=a;
+      a=c;
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+}
