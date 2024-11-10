22097edb50020c48a04976b4affb3381a2c72a5b
priyanshu <priyanshusengarps@gmail.com>
2024-11-10 10:28:22 +0530

    Basic JAVA Programs

---
+++ src/Program13.java
@@ -0,0 +1,8 @@
+public class Program13 {
+    public static void main(String[] args){
+        double height=8.5;
+        double weight=5.6;
+        System.out.println("Area is ="+height*weight);
+        System.out.println("Perimeter is ="+2*(height+weight));
+    }
+}
