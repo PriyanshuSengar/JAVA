d82fb0e2ee0b1ec383d5a7f2538f508f1729753a
priyanshu <priyanshusengarps@gmail.com>
2024-11-17 13:45:16 +0530

    Basic JAVA Programs

---
+++ src/Program43.java
@@ -0,0 +1,10 @@
+public class Program43 {
+    public static void main(String[] args){
+        System.out.println("Twinkle, twinkle, little star,\n" +
+                "\tHow I wonder what you are! \n" +
+                "\t\tUp above the world so high,   \t\t\n" +
+                "\t\tLike a diamond in the sky. \n" +
+                "Twinkle, twinkle, little star, \n" +
+                "\tHow I wonder what you are");
+    }
+}
