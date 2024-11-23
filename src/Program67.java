23a9e370cd4da80d31f15fca39aa53a678835796
priyanshu <priyanshusengarps@gmail.com>
2024-11-23 09:16:48 +0530

    Basic JAVA Programs

---
+++ src/Program67.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class Program67 {
+    public static void main(String[] args) {
+        String main_string = "Python 3.0";
+        String word = "Tutorial";
+        String result = main_string.substring(0, 7) + word + main_string.substring(6);
+        System.out.println(result);
+    }
+}
