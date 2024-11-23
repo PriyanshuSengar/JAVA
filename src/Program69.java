02015dae57aea636f5c28016f6ad016d6c1cd202
priyanshu <priyanshusengarps@gmail.com>
2024-11-23 09:20:09 +0530

    Basic JAVA Programs

---
+++ src/Program69.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+public class Program69 {
+    public static void main(String[] args) {
+        String main_string = "Python";
+        String substring = main_string.substring(0, main_string.length() / 2);
+        System.out.println(substring);
+    }
+}
