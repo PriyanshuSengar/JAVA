79c0b618411c80d4c286e8377d84a7e7d7757fa7
priyanshu <priyanshusengarps@gmail.com>
2024-11-27 21:57:32 +0530

    Basic JAVA Programs

---
+++ src/Program73.java
@@ -0,0 +1,13 @@
+public class Program73 {
+    public static void main(String[] args){
+        int[] array = {10, -20, 0, 30, 40, 60, 1};
+       int var=array[0];
+       int var2=array.length-1;
+       if(var == 10 || var2 == 10){
+           System.out.println("true");
+       }
+       else{
+           System.out.println("False");
+       }
+    }
+}
